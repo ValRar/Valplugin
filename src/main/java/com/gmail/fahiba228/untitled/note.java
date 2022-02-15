061d24bb511db5a883ed208d90d58982da1e9804
ValRar <val21219@gmail.com>
2022-02-15 14:11:34 +0300

    Deleted imports

--- src/main/java/com/gmail/fahiba228/untitled/note.java
+++ src/main/java/com/gmail/fahiba228/untitled/note.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.io.FileWriter;
 import java.io.IOException;
