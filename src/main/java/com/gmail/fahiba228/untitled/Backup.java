10361c940cb2da08d2ac86795612be926912170c
val21 <val21219@gmail.com>
2022-06-11 12:58:21 +0300

    Add some broadcasts

--- src/main/java/com/gmail/fahiba228/untitled/Backup.java
+++ src/main/java/com/gmail/fahiba228/untitled/Backup.java
@@ -10,35 +10,40 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Logger;
 
 public class Backup implements CommandExecutor {
     private final Main plugin;
     private final File backupDir;
+    private final Logger logger;
 
     public Backup(Main plugin, File backupDir) {
         this.plugin = plugin;
         this.backupDir = backupDir;
+        logger = plugin.getLogger();
     }
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        Bukkit.broadcastMessage(ChatColor.BOLD + "Start making backup...");
         String worldName = Bukkit.getWorlds().get(0).getName();
-        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd'_'HH-mm");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'_'HH-mm");
         Date date = new Date(System.currentTimeMillis());
         String zipName = worldName + "_" + formatter.format(date) + ".zip";
-        Bukkit.broadcastMessage(zipName);
         Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
             @Override
             public void run() {
                 try {
-                    Zipper zipper = new Zipper("./worldBackups/" + zipName);
+                    Zipper zipper = new Zipper("./worldBackups/" + zipName, logger);
                     zipper.addDirectory(new File(worldName));
                     zipper.addDirectory(new File(worldName + "_nether"));
                     zipper.addDirectory(new File(worldName + "_the_end"));
                     zipper.close();
-                    Bukkit.broadcastMessage(ChatColor.GREEN + "Backup of the world was created successfully.");
+                    logger.info(ChatColor.GREEN + "Backup of the world was created successfully.");
+                    commandSender.sendMessage(ChatColor.GREEN + "Backup of the world was created successfully.");
                 } catch (IOException e) {
-                    Bukkit.broadcastMessage(ChatColor.RED + "Failed to create a backup of the world!");
+                    commandSender.sendMessage(ChatColor.RED + "Failed to create a backup of the world!");
+                    logger.info(ChatColor.RED + "Failed to create a backup of the world!");
                     e.printStackTrace();
                 }
             }
